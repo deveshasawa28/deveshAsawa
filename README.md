d62c586fa4c141d58cdeebe01e975adeab62504e
deveshasawa28 <deveshasawa02@gmail.com>
2022-11-13 02:39:54 +0530

    completed website

---
+++ README.md
@@ -0,0 +1,35 @@
+# React Resume - Responsive and customizable CV / Resume
+
+You can easily create your fully beautiful CV / Resume by passing necessary props. <br/>
+Just clone or fork the repository and edit [data.js](https://github.com/Maaato/react-resume/blob/master/src/Schemas/Data.js) inside schemas folder and build your own CV.
+
+- Light / Dark mode 🌗
+- Snowflakes mode ❄️
+
+[Preview](https://react-resume-rho.vercel.app)
+
+## Getting Started 👋
+### Built With 🛠️
+
+- [React](https://es.reactjs.org)
+- [Boxicons](https://boxicons.com)
+- [React-particles-js](https://www.npmjs.com/package/react-particles-js)
+- [React-helmet](https://github.com/nfl/react-helmet)
+
+### Installation 🔧
+```
+$ yarn install
+> Edit Schemas/Data.js with your personal information
+```
+### Running the app 🏃
+```
+# dev mode
+$ yarn start or npm run start
+```
+
+### Features 💡
+- [Generate-CV-PDF](https://github.com/Maaato/react-resume/tree/feature/cv-pdf-generator)
+
+## License :memo:
+
+This project is [MIT](LICENCE) licensed.
---
+++ src/Pages/Resume.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+
+import { Profile } from "../Components/Profile";
+import { Academic } from "../Components/Academic";
+import { Skills } from "../Components/Skills";
+import { Proyects } from "../Components/Proyects";
+import { Works } from "../Components/Works";
+import { SocialMedia } from "../Components/SocialMedia";
+import { AboutMe } from "../Components/AboutMe";
+import { Menu } from "../Components/Menu";
+import { SEO } from "../Components/SEO";
+
+import { Data as dataSchema } from "../Schemas/Data";
+import { Menu as menuSchema } from "../Schemas/Menu";
+
+export const Resume = () => {
+  const query = "(min-width: 968px)";
+  const [matches, setMatches] = useState(window.matchMedia(query).matches);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    const listener = () => setMatches(media.matches);
+    media.addEventListener("change", listener);
+    return () => media.removeEventListener("change", listener);
+  }, [matches]);
+
+  const { profile, aboutMe, skills, socialMedia, experience } = dataSchema;
+
+  return (
+    <>
+      <SEO  {...profile} {...aboutMe}/>
+      {!matches && <Menu {...menuSchema} />}
+      <main className="l-main bd-container" id="bd-container">
+        <div className="resume" id="area-cv">
+          <div className="resume__left">
+            <Profile {...profile} />
+            <AboutMe {...aboutMe} />
+            <Skills {...skills} />
+            <SocialMedia {...socialMedia} />
+          </div>
+          <div className="resume__right">
+            <Works {...experience} />
+            <Academic {...experience} />
+            <Proyects {...experience} />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
