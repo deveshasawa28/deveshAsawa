997812987a4105b88416d944cc8fd90c8aa8fbdd
deveshasawa28 <deveshasawa02@gmail.com>
2022-11-13 02:39:54 +0530

    completed website

---
+++ src/Schemas/Data.js
@@ -0,0 +1,127 @@
+export const Data = {
+  profile: {
+    name: "Devesh Asawa",
+    ocupation: "FullStack Software Developer",
+    location: "village:- khinyala via-jayal Nagaur Rajasthan",
+    email: "deveshasawa02@gmail.com",
+    telephone: "+91 **********",
+    image: "https://media-exp1.licdn.com/dms/image/C4D03AQF4z17qzGa1Rw/profile-displayphoto-shrink_200_200/0/1659038158552?e=1673481600&v=beta&t=HYNATCw1akd6PV2BcBI1PAdQekKvvlEkwK6-hV6TrII",
+  },
+  aboutMe: {
+    label: "Profile",
+    description:
+      "Programmer Analyst, trained to develop computer applications in various technologies and multiple programming languages, applying agile development methodologies, including identification of needs and system designs based on development models.",
+  },
+  skills: {
+    technicalLabel: "TECHNOLOGIES",
+    softLabel: "Skills",
+    technicalSkills: [
+      "Nodejs",
+      "React",
+      "Sanity",
+      "Git",
+      "Angular",
+    ],
+    softSkills: [
+      'Analysis capacity',
+      'Problem resolution',
+      'Teamwork',
+      'Proactivity',
+      'Creativity'
+    ],
+  },
+  socialMedia: {
+    label: "SOCIAL",
+    social: [
+      {
+        label: "devesh asawa",
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/deveshasawa",
+        className: "bxl-linkedin-square",
+      },
+      // {
+      //   label: "Maaato",
+      //   name: "github",
+      //   url: "https://github.com/Maaato",
+      //   className: "bxl-github",
+      // },
+      // {
+      //   label: "Maaato",
+      //   name: "twiiter",
+      //   url: "https://twitter.com/I_Maaato",
+      //   className: "bxl-twitter",
+      // },
+    ],
+  },
+  experience: {
+    works: [
+      {
+        title: "FULL-STACK DEVELOPER",
+        period: "August. 2022 - Present ",
+        company: "Fulminous Software Private Limited.",
+        description: [
+          ""
+        ],
+      },
+      {
+        title: "FULL-STACK DEVELOPER",
+        period: "May. 2021 - August. 2022",
+        company: "Fulminous Software Private Limited.",
+        description: [
+          "",
+        ],
+      },
+      {
+        title: "FULL-STACK DEVELOPER",
+        period: "Oct. 2020 - March. 2021",
+        company: "Freelancing",
+        description: [
+          " ",
+        ],
+      },
+      {
+        title: "Intern",
+        period: "Feb. 2020 - July. 2020",
+        company: "Fulminous Software Private Limited.",
+        description: [
+          "",
+        ],
+      }
+    ],
+    academic: [
+      {
+        career: "Master of Computer Applications",
+        date: "2020",
+        institution: "University Of Kota Kota",
+      },
+    ],
+    projects: [
+      {
+        name: "Digital Onboarding",
+        company: "Fulminous Software Private Limited.",
+        period: "Nov. 2019 - Ene. 2020",
+        description: [
+          "This project is developed to ease the onboarding process of candidates.",
+          "It provides a platform to help new hired employees to complete onboarding process by uploading all the necessary document and signing the documents and accepting company polices.",
+        ],
+      },
+      {
+        name: "Verifier",
+        company: "Fulminous Software Private Limited.",
+        period: "Ene. 2020 - Feb. 2020",
+        description: [
+          "This was created to do background verification. ",
+          "We integrated auth-bridge with our application and made all the background verification automated. Backend was developed in NodeJS and frontend was developed in react.",
+        ],
+      },
+      {
+        name: "Tracking Crm",
+        company: "Fulminous Software Private Limited.",
+        period: "Mar. 2020 - May. 2022",
+        description: [
+          "This project was aimed to launch a real time application for examiner’s/worker’s live location record with visit details for multiple Tenant.",
+        ],
+      },
+    ],
+  },
+};
